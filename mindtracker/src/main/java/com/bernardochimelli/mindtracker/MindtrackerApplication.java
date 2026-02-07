470ae105180db4a854806deb873e891d327b0805
Bernardo Chimelli <bernardochimelliluz14@gmail.com>
2026-02-07 09:00:26 -0300

    Add mindtrack and mindtracker project skeletons
    Initial project scaffolding: add two Maven modules.
    
    - mindtrack: simple Java Main class, pom.xml and IDE config files (.idea/*) plus a .gitignore.
    - mindtracker: Spring Boot application skeleton with main class, application.properties, basic test, pom.xml, Maven wrapper (mvnw, mvnw.cmd, .mvn/wrapper/*), .gitignore and .gitattributes. README.md was moved into mindtracker/.
    
    This commit provides the base structure to start development for both modules.

---
+++ mindtracker/src/main/java/com/bernardochimelli/mindtracker/MindtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bernardochimelli.mindtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindtrackerApplication.class, args);
+	}
+
+}
---
+++ mindtracker/src/test/java/com/bernardochimelli/mindtracker/MindtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardochimelli.mindtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
